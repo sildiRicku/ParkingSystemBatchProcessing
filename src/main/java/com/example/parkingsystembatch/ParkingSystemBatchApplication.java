554371f4e86e4051aa52eb23db25999e75c61b85
sildiRicku <sildi.ricku@albanialab.al>
2023-09-06 11:32:16 +0200

    push to git

---
+++ src/main/java/com/example/parkingsystembatch/ParkingSystemBatchApplication.java
@@ -0,0 +1,12 @@
+package com.example.parkingsystembatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSystemBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingSystemBatchApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/parkingsystembatch/ParkingSystemBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingsystembatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSystemBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
